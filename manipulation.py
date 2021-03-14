82bea7f4b513286774cf2d844e4f162d2e76f2c6
wkdewey <wkdewey@gmail.com>
2021-03-14 11:40:48 -0400

    print out child elements and so forth of xml

--- manipulation.py
+++ manipulation.py
@@ -3,3 +3,10 @@ tree = ET.parse('movies.xml')
 root = tree.getroot()
 for child in root:
     print(child.tag, child.attrib)
+for movie in root.iter('movie'):
+    print(movie.attrib)
+    for description in root.iter('description'):
+    print(description.text)
+
+elements = [elem.tag for elem in root.iter()]
+print(elements)
