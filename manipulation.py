d8fe023192b53d23f5bdd502b484b07699a81ab9
wkdewey <wkdewey@gmail.com>
2021-03-14 11:43:19 -0400

    fix indentation error

--- manipulation.py
+++ manipulation.py
@@ -5,7 +5,7 @@ for child in root:
     print(child.tag, child.attrib)
 for movie in root.iter('movie'):
     print(movie.attrib)
-    for description in root.iter('description'):
+for description in root.iter('description'):
     print(description.text)
 
 elements = [elem.tag for elem in root.iter()]
